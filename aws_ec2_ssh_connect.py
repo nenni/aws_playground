e5f00cdd2686fbfee3e3fff6c985b6eccc09945f
Nikolay Nenchev <nikolay.nenchev@gmail.com>
2016-04-01 16:18:00 +0100

    positional args for pem file and OS user

--- aws_ec2_ssh_connect.py
+++ aws_ec2_ssh_connect.py
@@ -3,57 +3,67 @@
 import sys
 import subprocess
 import boto3
-
+import argparse
+from argparse import RawTextHelpFormatter
 
 SSH_BIN = '/usr/bin/ssh'
-AWS_USER = 'ec2-user'
-AWS_CERT = '~/Projects/AWS/nenni/n_aws_linux_micro_1.pem'
 SSH_TMOUT = 20
 
 
 def main():
     try:
+        pem_file, ec2_user = arg_params()
         list_instances = get_aws_node_ip()
 
         if list_instances:
-            user_input = int(input("Please select instance id to connect to: "))
-            if 0 <= user_input < len(list_instances):
-                host = list_instances[user_input]
+            user_input = int(input("Please select instance id to connect to (0 to quit): "))
+            if user_input == 0:
+                exit(2)
 
-        ssh_cmd = '{ssh_bin} -i {aws_cert} {aws_user}@{host}'.format(
-            ssh_bin=SSH_BIN,
-            aws_cert=AWS_CERT,
-            aws_user=AWS_USER,
-            host=host
-        )
-
-        print(ssh_cmd)
-        subprocess.run(ssh_cmd, timeout=SSH_TMOUT, shell=True)
-
+            # print(list_instances)
+            if 1 <= user_input < (len(list_instances)+1):
+                host = list_instances[user_input-1]
+                # print(host)
+                ssh_cmd = '{ssh_bin} -i {aws_cert} {aws_user}@{host}'.format(
+                    ssh_bin=SSH_BIN,
+                    aws_cert=pem_file,
+                    aws_user=ec2_user,
+                    host=host
+                )
+                print(ssh_cmd)
+                subprocess.run(ssh_cmd, timeout=SSH_TMOUT, shell=True)
     except Exception as e:
-        print("INVALID INPUT")
+        print("INVALID INPUT", str(e))
         return 1
 
+
+def arg_params():
+    parser = argparse.ArgumentParser(description="AWS EC2 SSH connection script",
+                                     formatter_class=RawTextHelpFormatter)
+    parser.add_argument('-p', dest="pem_file", help='ssh PEM file', required=True)
+    parser.add_argument('-u', dest="user", help='AWS EC2 instance user', required=True)
+    args = parser.parse_args()
+    pem_file = args.pem_file
+    ec2_user = args.user
+
+    return pem_file, ec2_user
 
 
 def get_aws_node_ip():
     ec2 = boto3.resource('ec2')
     instances = ec2.instances.all()
-    instance_id = 0
+    instance_id = 1
 
     list_instances_ip = []
     for instance in instances:
         list_instances_ip.append(instance.public_ip_address)
-
         print("{id}) {hostname} | State: {state} | Public IP/FQDN: {pub_fqdn}".format(
             id=instance_id,
             hostname=instance.tags[0].get('Value', None),
             state=str(instance.state["Name"]).upper(),
             pub_fqdn=instance.public_dns_name,
         ))
-
         instance_id += 1
-
     return list_instances_ip
 
 
