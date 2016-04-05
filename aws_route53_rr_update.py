bfd30a9d7e79c13f57e5a861ce1faa5d987b6816
Nikolay Nenchev <nikolay.nenchev@gmail.com>
2016-04-05 20:31:15 +0100

    update local public ipv4 address fetched from aws ec2 metadata

--- aws_route53_rr_update.py
+++ aws_route53_rr_update.py
@@ -3,12 +3,14 @@
 import sys
 import boto3
 from pprint import pprint
+import requests
 
 
 DNS_RR_TYPE = 'A'
 DNS_RR = 'neniko.eu.'
 DNS_ZONE = DNS_RR
-L_PUBLIC_IPv4 = '52.37.190.179'
+URL_META = 'http://169.254.169.254/latest/meta-data/public-ipv4'
+URL_TMOUT = 5
 
 
 def main():
@@ -17,7 +19,11 @@ def main():
 
         response_hostedzone = client.list_hosted_zones_by_name(DNSName=DNS_ZONE)
         hostedzoneid = response_hostedzone['HostedZones'][0]['Id']
-        print(hostedzoneid)
+        print("Hosted Zone id: " + hostedzoneid)
+
+        response = requests.get(URL_META, timeout=URL_TMOUT)
+        local_pub_ip = response.content.decode("utf-8")
+        print("Local Public IPv4 address: " + local_pub_ip)
 
         dns_rrs = client.list_resource_record_sets(HostedZoneId=hostedzoneid,
                                                    StartRecordName=DNS_RR,
@@ -27,22 +33,24 @@ def main():
         if dns_rrs['ResourceRecordSets']:
             for dns_rr in dns_rrs['ResourceRecordSets']:
                 if dns_rr['Type'] == DNS_RR_TYPE and dns_rr['Name'] == DNS_RR:
-                    rr = dns_rr
                     rr_ip = dns_rr['ResourceRecords'][0]['Value']
-                    print("Host zone RR: " + str(rr))
+                    print("Current Host zone RR: " + str(dns_rr))
 
-                    if L_PUBLIC_IPv4 != rr_ip:
-                        new_rr = rr
-                        new_rr['ResourceRecords'][0]['Value'] = L_PUBLIC_IPv4
-                        pprint(new_rr)
+                    if rr_ip != local_pub_ip:
+                        dns_rr['ResourceRecords'][0]['Value'] = local_pub_ip
+                        # pprint(dns_rr)
                         response = client.change_resource_record_sets(
                             HostedZoneId=hostedzoneid,
                             ChangeBatch={'Changes': [
                                 {'Action': 'UPSERT',
-                                 'ResourceRecordSet': new_rr
+                                 'ResourceRecordSet': dns_rr
                                  }
                             ]})
+                        # print("Response code: " + str(response['ResponseMetadata']['HTTPStatusCode']))
+                        # print("Response status: " + response['ChangeInfo']['Status'])
                         pprint(response)
+                    else:
+                        print("DNS address is the same as the local public ipv4 address. Change is not required.")
 
     except Exception as e:
         print("INVALID INPUT", str(e))
