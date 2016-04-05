2e95e950869e32b99d84f4f980ebd547d77fc6b7
Nikolay Nenchev <nikolay.nenchev@gmail.com>
2016-04-05 23:18:53 +0100

    update: split the code into more functions

--- aws_route53_rr_update.py
+++ aws_route53_rr_update.py
@@ -4,6 +4,7 @@ import sys
 import boto3
 from pprint import pprint
 import requests
+import socket
 
 
 DNS_RR_TYPE = 'A'
@@ -15,47 +16,80 @@ URL_TMOUT = 5
 
 def main():
     try:
+
+        local_pub_ip = ec2_local_pub_ip()
         client = boto3.client('route53')
-
-        response_hostedzone = client.list_hosted_zones_by_name(DNSName=DNS_ZONE)
-        hostedzoneid = response_hostedzone['HostedZones'][0]['Id']
-        print("Hosted Zone id: " + hostedzoneid)
-
-        response = requests.get(URL_META, timeout=URL_TMOUT)
-        local_pub_ip = response.content.decode("utf-8")
-        print("Local Public IPv4 address: " + local_pub_ip)
-
-        dns_rrs = client.list_resource_record_sets(HostedZoneId=hostedzoneid,
-                                                   StartRecordName=DNS_RR,
-                                                   StartRecordType=DNS_RR_TYPE,
-                                                   MaxItems='1')
-
-        if dns_rrs['ResourceRecordSets']:
-            for dns_rr in dns_rrs['ResourceRecordSets']:
-                if dns_rr['Type'] == DNS_RR_TYPE and dns_rr['Name'] == DNS_RR:
-                    rr_ip = dns_rr['ResourceRecords'][0]['Value']
-                    print("Current Host zone RR: " + str(dns_rr))
-
-                    if rr_ip != local_pub_ip:
-                        dns_rr['ResourceRecords'][0]['Value'] = local_pub_ip
-                        # pprint(dns_rr)
-                        response = client.change_resource_record_sets(
-                            HostedZoneId=hostedzoneid,
-                            ChangeBatch={'Changes': [
-                                {'Action': 'UPSERT',
-                                 'ResourceRecordSet': dns_rr
-                                 }
-                            ]})
-                        # print("Response code: " + str(response['ResponseMetadata']['HTTPStatusCode']))
-                        # print("Response status: " + response['ChangeInfo']['Status'])
-                        pprint(response)
-                    else:
-                        print("DNS address is the same as the local public ipv4 address. Change is not required.")
+        hosted_zone_id, dns_rrs = get_dns_rrs(client)
+        change_dns_rr(client, hosted_zone_id, dns_rrs, local_pub_ip)
 
     except Exception as e:
         print("INVALID INPUT", str(e))
         return 1
 
+
+def change_dns_rr(client, hostedzoneid, dns_rrs, local_pub_ip):
+    """
+
+    :param client:
+    :param hostedzoneid:
+    :param dns_rrs:
+    :return:
+    """
+    if dns_rrs['ResourceRecordSets']:
+        for dns_rr in dns_rrs['ResourceRecordSets']:
+            if dns_rr['Type'] == DNS_RR_TYPE and dns_rr['Name'] == DNS_RR:
+                rr_ip = dns_rr['ResourceRecords'][0]['Value']
+                print("Current Host zone RR: " + str(dns_rr))
+
+                if rr_ip != local_pub_ip:
+                    dns_rr['ResourceRecords'][0]['Value'] = local_pub_ip
+                    # pprint(dns_rr)
+                    response = client.change_resource_record_sets(
+                        HostedZoneId=hostedzoneid,
+                        ChangeBatch={'Changes': [
+                            {'Action': 'UPSERT',
+                             'ResourceRecordSet': dns_rr
+                             }
+                        ]})
+                    # print("Response code: " + str(response['ResponseMetadata']['HTTPStatusCode']))
+                    # print("Response status: " + response['ChangeInfo']['Status'])
+                    pprint(response)
+                else:
+                    print("DNS address is the same as the local public ipv4 address. Change is not required.")
+
+
+def get_dns_rrs(client):
+    """
+
+    :param client:
+    :return:
+    """
+    response_hosted_zone = client.list_hosted_zones_by_name(DNSName=DNS_ZONE)
+    hosted_zone_id = str(response_hosted_zone['HostedZones'][0]['Id'])
+    print("Hosted Zone id: " + hosted_zone_id)
+
+    dns_rrs = client.list_resource_record_sets(HostedZoneId=hosted_zone_id,
+                                               StartRecordName=DNS_RR,
+                                               StartRecordType=DNS_RR_TYPE,
+                                               MaxItems='1')
+    return hosted_zone_id, dns_rrs
+
+
+def ec2_local_pub_ip():
+    """
+
+    :return:
+    """
+    try:
+        response = requests.get(URL_META, timeout=URL_TMOUT)
+        ipaddress = response.content.decode("utf-8")
+        print("Local Public IPv4 address: " + ipaddress)
+
+        socket.inet_aton(ipaddress)
+        return ipaddress
+
+    except socket.error as e:
+        raise ValueError(e)
 
 if __name__ == '__main__':
     sys.exit(main())
